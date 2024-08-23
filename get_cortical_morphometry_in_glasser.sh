22216903c51fe8b1392e7f908355247c72b5719b
KobaCemal <45714553+KobaCemal@users.noreply.github.com>
2024-08-23 12:29:02 +0200

    Update get_cortical_morphometry_in_glasser.sh

--- get_cortical_morphometry_in_glasser.sh
+++ get_cortical_morphometry_in_glasser.sh
@@ -7,6 +7,6 @@ do
         echo " "
         echo "=========================== Processing $sub $hemi ==========================="
         mris_anatomical_stats -a "$SUBJECTS_DIR"/"$sub"/label/"$hemi".glasser-360_mics.annot -b "$sub" "$hemi" > "$SUBJECTS_DIR"/"$sub"/stats/"$hemi".glasser.stats
-        mris_anatomical_stats -a "$SUBJECTS_DIR"/"$sub"/label/"$hemi".glasser-360_mics.annot -t "$SUBJECTS_DIR"/"$sub"/surf/lh.sulc -b "$sub" "$hemi"  > "$SUBJECTS_DIR"/"$sub"/stats/"$hemi".glasser_sulc.stats
+        mris_anatomical_stats -a "$SUBJECTS_DIR"/"$sub"/label/"$hemi".glasser-360_mics.annot -t "$SUBJECTS_DIR"/"$sub"/surf/"$hemi".sulc -b "$sub" "$hemi"  > "$SUBJECTS_DIR"/"$sub"/stats/"$hemi".glasser_sulc.stats
     done
 done
