042b6c3e6bbeaa68bbcaf6fe3354a28f807851d1
KobaCemal <45714553+KobaCemal@users.noreply.github.com>
2024-09-03 12:06:50 +0200

    Update and rename Group_analysis_gradients_wholerain.m to Group_analysis_gradients_wholebrain.m

--- Group_analysis_gradients_wholebrain.m
+++ Group_analysis_gradients_wholebrain.m
@@ -187,10 +187,8 @@ for i=1:size(lambdas,2)
     % controlXcb_d(i,j)=abs(computeCohen_d(squeeze(residuals_cbset(i,j,1:14)),squeeze(residuals_cbset(i,j,15:end))));
 
 end
-tbl=table(sex(1:71),age(1:71),group(1:71),site(1:71,1),range(1:71,2))
-mdl = fitlm(tbl, 'Var5 ~ Var1 + Var2 * Var3', 'CategoricalVars', {'Var1', 'Var3'})
-plotInteraction(mdl, 'Var2', 'Var3');
 
+% variances
 clear controlXcb_t controlXcb_p
 vars=squeeze(var(gradients_all,[],2));
 residuals=zeros(size(vars));
@@ -231,49 +229,10 @@ end
 fdr_bh(controlXcb_p)
 
 % Correlation between range and lambdas
-[a,b]=corr(lambdas(:,1),range(:,1),'Type',"Spearman")
-[a,b]=corr(lambdas(:,2),range(:,2),'Type',"Spearman")
-[a,b]=corr(lambdas(:,3),range(:,3),'Type',"Spearman")
+[a,b]=corr(lambdas(strcmp({parameters_subset.group}','SC'),1),range(strcmp({parameters_subset.group}','SC'),1),'Type',"Spearman")
+[a,b]=corr(lambdas(strcmp({parameters_subset.group}','SC'),2),range(strcmp({parameters_subset.group}','SC'),2),'Type',"Spearman")
+[a,b]=corr(lambdas(strcmp({parameters_subset.group}','SC'),3),range(strcmp({parameters_subset.group}','SC'),3),'Type',"Spearman")
 
-
-
-
-
-scatter([parameters_subset.age]',range(:,2))
-lsline
-
-[r,p]=corr([parameters_subset(strcmp({parameters_subset.group}','SC')).age]',range(strcmp({parameters_subset.group}','SC'),2))
-[r,p]=corr([parameters_subset(strcmp({parameters_subset.group}','CB')).age]',range(strcmp({parameters_subset.group}','CB'),2))
-
-% Correlation with age
-ix=1:71;
-age_nokrk=age(ix);
-range_nokrk=range(ix,:);
-prm_nokrk=parameters_subset(ix);
-sex_nokrk=sex(ix);
-site_nokrk=site(ix,1);
-group_nokrk=dummyvar(grp2idx({prm_nokrk.group}'));
-group_nokrk=group_nokrk(:,1);
-mdl=fitlm([ sex_nokrk age_nokrk group_nokrk site_nokrk],range_nokrk(:,2));site_nokrk;
-mdl.Coefficients
-% covars=[ones(size(age_nokrk,1),1) sex_nokrk site_nokrk(:,1)];
-% [~,~,residuals_age] = regress(age_nokrk,covars);
-% clear residuals_range
-% for i=1:10
-%     [~,~,r] = regress(range_nokrk(:,i),covars);
-%     residuals_range(:,i)=r;
-% end
-
-fitlm(X,y)
-
-% for i=1:10
-%     [a,b]=corr([residuals_age(strcmp({prm_nokrk.group}','CB'))],residuals_range(strcmp({prm_nokrk.group}','CB'),i),'Type','Spearman');
-%     ff(i,1)=a;
-%     ff(i,2)=b;
-%     [a,b]=corr([residuals_age(strcmp({prm_nokrk.group}','SC'))],residuals_range(strcmp({prm_nokrk.group}','SC'),i),'Type','Spearman');
-%     ff(i,3)=a;
-%     ff(i,4)=b;
-% end
 
 %% Rank and distance
 
@@ -313,7 +272,7 @@ for i=1:size(ranks,2)
         CB_group=(ranks(strcmp({parameters_subset.group}','CB'),i,j));
 
 
-        [p,h,stats] = ranksum(SC_group,CB_group);
+        [p,h,stats] = ranksum(CB_group,SC_group);
         ranks_p(i,j)=p;
         ranks_z(i,j)=stats.zval;
 
@@ -327,19 +286,18 @@ for i=1:size(ranks,2)
         SC_freq = histcounts(SC_group, 0.5:4.5);
         CB_freq = histcounts(CB_group, 0.5:4.5);
 
-        % Create a contingency table
         contingency_tables{i,j} = [SC_freq; CB_freq]';
     end
 end
-
+fdr_bh(ranks_p(:,1:3)).*ranks_z(:,1:3)
 tbl=table(ranks(:,:,1),{parameters_subset.group}');
 tbl=sortrows(tbl,"Var2","descend");
 imagesc(tbl.Var1')
 
-%================ Distance
+%================ Min Distance
 for  i=1:size(gradients_all,1)
     for j=1:10
-        values=[residuals(i,strcmp(glasser_metadata.regionName,'V1_L'),j) gradients_all(i,strcmp(glasser_metadata.regionName,'V1_R'),j);...
+        values=[gradients_all(i,strcmp(glasser_metadata.regionName,'V1_L'),j) gradients_all(i,strcmp(glasser_metadata.regionName,'V1_R'),j);...
             gradients_all(i,strcmp(glasser_metadata.regionName,'V2_L'),j) gradients_all(i,strcmp(glasser_metadata.regionName,'V2_R'),j);...
             gradients_all(i,strcmp(glasser_metadata.regionName,'V3_L'),j) gradients_all(i,strcmp(glasser_metadata.regionName,'V3_R'),j);...
             gradients_all(i,strcmp(glasser_metadata.regionName,'V4_L'),j) gradients_all(i,strcmp(glasser_metadata.regionName,'V4_R'),j)];
@@ -356,57 +314,38 @@ end
 % imagesc(min_distances(:,1,1)')
 for  i=1:size(min_distances,2)
     for j=1:10
-        [h,p,cc,stats]=ttest2(min_distances(strcmp(group,'SC'),i,j),...
-            min_distances(strcmp(group,'CB'),i,j));
+        [h,p,cc,stats]=ttest2(min_distances(strcmp(group,'CB'),i,j),...
+            min_distances(strcmp(group,'SC'),i,j));
         distances_t(i,j)=stats.tstat;
         distances_p(i,j)=p;
     end
 end
+fdr_bh(distances_p(:,1:3)).*distances_t(:,1:3)
+%% Structural measures
 
-%% Structural stuff
 volume = [parameters_subset.volume]';
 thickness = [parameters_subset.thickness_fs].';
-% curvature_int = [parameters_subset.intrinsic_curvature].';
-% curvature_rect = [parameters_subset.rectified_curvature].';
-% curvature_rect_gauss = [parameters_subset.rectified_curvature_gaussian].';
 surface_area = [parameters_subset.surface_area]';
-% folding_index = [parameters_subset.folding_index]';
 sulcal_depth = [parameters_subset.sulcal_depth]';
 curvature_roi_mp = [parameters_subset.curvature_roi_mp]';
-% t_mp=[parameters_subset.thickness_roi_mp]';
-% c_mp=[parameters_subset.curvature_roi_mp]';
-% 
-% l_thal=[parameters_subset.L_Thal]';
-% r_thal=[parameters_subset.R_Thal]';
-% l_hipp=[parameters_subset.L_Hippo]';
-% r_hipp=[parameters_subset.R_Hippo]';
 
 
 
 % comparison at group level
 covars=[ones(size(thickness,1),1) age sex];
-% residuals=zeros(size(curvature_roi));
+ 
 for i=1:size(thickness,2)
     [~,~,r] = regress(volume(:,i),covars);
     residuals_vl(:,i)=r;
 
     [~,~,r] = regress(thickness(:,i),covars);
     residuals_th(:,i)=r;
-    % 
-    % [~,~,r] = regress(curvature_int(:,i),covars);
-    % residuals_ci(:,i)=r;
-    % 
-    % [~,~,r] = regress(curvature_rect(:,i),covars);
-    % residuals_cr(:,i)=r;
 
     [~,~,r] = regress(curvature_roi_mp(:,i),covars);
     residuals_curv(:,i)=r;
 
     [~,~,r] = regress(surface_area(:,i),covars);
     residuals_sa(:,i)=r;
-
-    % [~,~,r] = regress(folding_index(:,i),covars);
-    % residuals_fi(:,i)=r;
 
     [~,~,r] = regress(sulcal_depth(:,i),covars);
     residuals_sd(:,i)=r;
@@ -418,13 +357,6 @@ for i=1:size(thickness,2)
         residuals_sd(strcmp({parameters_subset.group}','SC'),i));
     controlXcb_p_sd(i)=p;
     controlXcb_t_sd(i)=stats.tstat;
-
-
-    % [h,p,cc,stats]=ttest2(residuals_fi(strcmp({parameters_subset.group}','CB'),i),...
-    %     residuals_fi(strcmp({parameters_subset.group}','SC'),i));
-    % controlXcb_p_fi(i)=p;
-    % controlXcb_t_fi(i)=stats.tstat;
-
 
     [h,p,cc,stats]=ttest2(residuals_vl(strcmp({parameters_subset.group}','CB'),i),...
         residuals_vl(strcmp({parameters_subset.group}','SC'),i));
@@ -441,16 +373,6 @@ for i=1:size(thickness,2)
     controlXcb_p_ci(i)=p;
     controlXcb_t_ci(i)=stats.tstat;
 
-    % [h,p,cc,stats]=ttest2(residuals_cr(strcmp({parameters_subset.group}','CB'),i),...
-    %     residuals_cr(strcmp({parameters_subset.group}','SC'),i));
-    % controlXcb_p_cr(i)=p;
-    % controlXcb_t_cr(i)=stats.tstat;
-    % 
-    % [h,p,cc,stats]=ttest2(residuals_crg(strcmp({parameters_subset.group}','CB'),i),...
-    %     residuals_crg(strcmp({parameters_subset.group}','SC'),i));
-    % controlXcb_p_crg(i)=p;
-    % controlXcb_t_crg(i)=stats.tstat;
-
     [h,p,cc,stats]=ttest2(residuals_sa(strcmp({parameters_subset.group}','CB'),i),...
         residuals_sa(strcmp({parameters_subset.group}','SC'),i));
     controlXcb_p_sa(i)=p;
@@ -464,28 +386,17 @@ plot_hemispheres([(fdr_bh(controlXcb_p_vl).*controlXcb_t_vl)' ...
     {surf_lh,surf_rh},'parcellation',glasser_label,'labeltext',{ 'Volume', 'Thickness','Surface Area','Sulcal Depth', 'Curvature'});
 
 
-
 for i=1:size(parameters_subset,2)
-    % structural_matrices(i,:,:) = [volume(i,:)' thickness(i,:)' curvature_int(i,:)' curvature_rect(i,:)' curvature_rect_gauss(i,:)' surface_area(i,:)' folding_index(i,:)'];
-    % structural_conn_matrices(i,:,:)=corr(squeeze(structural_matrices(i,:,:))');
-    structural_matrices(i,:,:) = [volume(i,:)' thickness(i,:)' surface_area(i,:)'  curvature_roi_mp(i,:)' sulcal_depth(i,:)']
-    
+
     [coeff,score,latent,tsquared,explained] = pca([volume(i,:)' thickness(i,:)' surface_area(i,:)'  curvature_roi_mp(i,:)' sulcal_depth(i,:)']);
     %
     pca_str(i,:)=score(:,1);
     pca_var(i,:)=explained;
 end
+
 plot_hemispheres([mean(pca_str(strcmp({parameters_subset.group}','SC'),:))'...
     mean(pca_str(strcmp({parameters_subset.group}','CB'),:))'], {surf_lh,surf_rh},'parcellation',glasser_label);
-% residuals=zeros(size(pca_str));
-% covars=[ones(size(residuals,1),1) age sex];
-% for i=1:size(residuals,2)
-%         [~,~,r] = regress(pca_str(:,i),covars);
-%         residuals(:,i)=r;
-% end
-parameters_2=parameters_subset(1:71);
 
-batch=(grp2idx({parameters_2.site}'));
 
 clear controlXcb_t controlXcb_p
 for i=1:size(pca_str,2)
@@ -497,6 +408,11 @@ for i=1:size(pca_str,2)
 end
 plot_hemispheres(((controlXcb_p<0.05).*controlXcb_t)', {surf_lh,surf_rh},'parcellation',glasser_label);
 %
+str_results=[((controlXcb_p<0.05).*controlXcb_t)' (fdr_bh(controlXcb_p_vl).*controlXcb_t_vl)' ...
+    (fdr_bh(controlXcb_p_th).*controlXcb_t_th)'...
+    (fdr_bh(controlXcb_p_sa).*controlXcb_t_sa)'...
+    (fdr_bh(controlXcb_p_sd).*controlXcb_t_sd)'...
+    (fdr_bh(controlXcb_p_ci).*controlXcb_t_ci)']
 
 
 residuals=zeros(size(gradients_all(1:71,:,:)));
@@ -507,26 +423,23 @@ for i=1:size(residuals,2)
         residuals(:,i,j)=r;
     end
 end
+parameters_2=parameters_subset(1:71);
 
+batch=(grp2idx({parameters_2.site}'));
 structural_harmonized = combat(pca_str(1:71,:)', batch, [age(1:71) sex(1:71)], 1)';
-% When checked alone, volume and sulcal depth gave significant differences
 
 for i=1:360
     for j=1:10
-        xx(i,j)=corr(residuals(strcmp({parameters_2.group}','SC'),i,j),structural_harmonized(strcmp({parameters_2.group}','SC'),j));
-        yy(i,j)=corr(residuals(strcmp({parameters_2.group}','CB'),i,j),structural_harmonized(strcmp({parameters_2.group}','CB'),j));
+        xx(i,j)=corr(residuals(strcmp({parameters_2.group}','CB'),i,j),structural_harmonized(strcmp({parameters_2.group}','CB'),j));
+        yy(i,j)=corr(residuals(strcmp({parameters_2.group}','SC'),i,j),structural_harmonized(strcmp({parameters_2.group}','SC'),j));
     end
 end
-% plot_hemispheres([xx(:,1) yy(:,1) xx(:,1)-yy(:,1)], {surf_lh,surf_rh},'parcellation',glasser_label);
-% plot_hemispheres([xx(:,2) yy(:,2) xx(:,2)-yy(:,2)], {surf_lh,surf_rh},'parcellation',glasser_label);
-
-
 for i=1:360
     for j=1:3
         r1 = xx(i,j); % Correlation coefficient for group 1
         r2 = yy(i,j); % Correlation coefficient for group 2
-        n1 = 44; % Sample size for group 1
-        n2 = 27;
+        n1 = 27; % Sample size for group 1
+        n2 = 44;
         % Fisher Z-transformation
         z1 = 0.5 * log((1 + r1) / (1 - r1));
         z2 = 0.5 * log((1 + r2) / (1 - r2));
@@ -536,8 +449,7 @@ for i=1:360
         pvals(i,j)=2 * (1 - normcdf(abs(z_diff)));
     end
 end
-plot_hemispheres([xx(:,1) yy(:,1) double(fdr_bh(pvals(:,1))).*(xx(:,1)-yy(:,1))], {surf_lh,surf_rh},'parcellation',glasser_label,'labeltext', {'SC','CB', 'Difference'});
-plot_hemispheres([xx(:,2) yy(:,2) double(fdr_bh(pvals(:,2))).*(xx(:,2)-yy(:,2))], {surf_lh,surf_rh},'parcellation',glasser_label,'labeltext', {'SC','CB', 'Difference'});
-plot_hemispheres([xx(:,3) yy(:,3) double(fdr_bh(pvals(:,3))).*(xx(:,3)-yy(:,3))], {surf_lh,surf_rh},'parcellation',glasser_label,'labeltext', {'SC','CB', 'Difference'});
-
+plot_hemispheres([xx(:,1) yy(:,1) (xx(:,1)-yy(:,1))], {surf_lh,surf_rh},'parcellation',glasser_label,'labeltext', {'CB','SC', 'Difference'});
+plot_hemispheres([xx(:,2) yy(:,2) (xx(:,2)-yy(:,2))], {surf_lh,surf_rh},'parcellation',glasser_label,'labeltext', {'CB','SC', 'Difference'});
+plot_hemispheres([xx(:,3) yy(:,3) (xx(:,3)-yy(:,3))], {surf_lh,surf_rh},'parcellation',glasser_label,'labeltext', {'CB','SC', 'Difference'});
 
